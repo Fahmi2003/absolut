8600858eab370da56dadb2eb6783f5c95675cadd
Fahmi2003 <60843418+Fahmi2003@users.noreply.github.com>
2020-09-07 22:26:38 +0700

    p

--- userbot/helpers/functions.py
+++ userbot/helpers/functions.py
@@ -5,7 +5,6 @@ from random import choice
 from telethon import events
 from emoji import get_emoji_regexp
 from PIL import Image
-from validators.url import url
 from telethon.tl.types import Channel
 
 async def get_readable_time(seconds: int) -> str:
