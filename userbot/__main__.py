4d26eda38131bdda6ca1cb4d8deafe336a0bdf77
Fahmi <febriantofahmi20@gmail.com>
2020-09-07 21:56:32 +0700

    ya

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,36 +1,56 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot start point """
-
-from importlib import import_module
+from userbot import bot
 from sys import argv
-
+import sys
+from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import LOGS, bot
-from userbot.modules import ALL_MODULES
+import os
+from telethon import TelegramClient
+from var import Var
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, LOGS
+from pathlib import Path
+import asyncio
+import telethon.utils
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
 
 
-INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
-             '\n Tip: Use Country Code along with number.' \
-             '\n or check your phone number and try again !'
 
-try:
-    bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    exit(1)
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+    
 
-for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
+import glob
+path = 'userbot/plugins/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
 
-LOGS.info("You are running One4uBot [v3.7]")
+import userbot._core
 
-LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
-    "If you need assistance, head to https://t.me/userbotindo")
+print("Yay your userbot is officially working.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
